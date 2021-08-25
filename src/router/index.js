8bb2304142173fe43e07a55c111ae81ad6ef5c0e
jansevero <46618122+jansevero@users.noreply.github.com>
2021-08-25 14:43:53 -0300

    config firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -18,14 +17,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Login
   }
 ]
 
